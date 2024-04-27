6f5c2772e63c44b175fdf9ee672a039ae74830de
Álefe Cruz <126995086+alefecruz@users.noreply.github.com>
2024-04-26 23:50:00 -0300

    build: config eslint, prettier and editorconfig

---
+++ metro.config.js
@@ -0,0 +1,7 @@
+/* eslint-env node */
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
