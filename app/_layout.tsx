5b7a4c0d62fd2b1280aec748ae124f3c46e24ad6
Álefe Cruz <126995086+alefecruz@users.noreply.github.com>
2024-04-27 02:13:04 -0300

    feat: set theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,10 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { ThemeProvider, themeRender } from '@/libs/style';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -45,10 +44,8 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider theme={themeRender({ color: 'default', size: 'default', spacing: 'default' })}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -1,4 +1,4 @@
-import { Text, TextProps } from './Themed';
+import { Text, TextProps } from 'react-native';
 
 export function MonoText(props: TextProps) {
   return <Text {...props} style={[props.style, { fontFamily: 'SpaceMono' }]} />;
